98cd37aa44394a2390819514f41e0b3ef58688c6
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-25 11:38:14 +0800

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -1,4 +1,3 @@
 <?php
 $meta['highlight_namespace']      = "bookmarks:highlight:";
 $meta['readability']      = '1';
-$meta['allowed_html_tags']      = "<html><head><sub><sup><u><frame><svg><table><th><tr><td><col><tfoot><thead><span><title><body><a><font><dt><img><br><code><data><canvas><li><p><h1><h2><h3><h4><h5>";
