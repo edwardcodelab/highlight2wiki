77379e845155dbce1c7d5b19efeaf9a5762f68f0
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-28 02:29:37 +0800

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,5 +1,4 @@
 <?php
 $meta['highlight_namespace']      = array('string');
-$meta['allowed_tags']             = array('string');
 $meta['allow_css'] = array('onoff');
 $meta['allow_javascript'] = array('onoff');
