ab6002f763e5534128b04ffa311beb7471958468
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-27 11:56:43 +0800

    Update action.php

--- action.php
+++ action.php
@@ -136,6 +136,7 @@ foreach($dom->getElementsByTagName('*') as $node)
 
 $html = $dom->saveHTML($dom->getElementsByTagname('html')->item(0));
 $html = strip_tags($result, $allowed_tags);
+$html = strip_tags_content($html,,true);
 echo $html;
         
 
@@ -221,6 +222,33 @@ function crc64Table() //CRC64 hasing for encoding
     return $crc64tab;
 }
  
+
+
+//https://www.php.net/manual/en/function.str-replace.php#100871
+function strip_tags_content($text, $tags = '', $invert = FALSE) {
+
+  preg_match_all('/<(.+?)[\s]*\/?[\s]*>/si', trim($tags), $tags);
+  $tags = array_unique($tags[1]);
+   
+  if(is_array($tags) AND count($tags) > 0) {
+    if($invert == FALSE) {
+      return preg_replace('@<(?!(?:'. implode('|', $tags) .')\b)(\w+)\b.*?>.*?</\1>@si', '', $text);
+    }
+    else {
+      return preg_replace('@<('. implode('|', $tags) .')\b.*?>.*?</\1>@si', '', $text);
+    }
+  }
+  elseif($invert == FALSE) {
+    return preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', $text);
+  }
+  return $text;
+}
+
+
+
+
+
+
 /**
 * @param string $string
 * @param string $format
