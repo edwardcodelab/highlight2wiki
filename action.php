b1deb5e131db2c44e241b843d61a79da1ce9dc7b
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-27 10:53:59 +0800

    Update action.php

--- action.php
+++ action.php
@@ -135,7 +135,8 @@ foreach($dom->getElementsByTagName('*') as $node)
 }
 
 $html = $dom->saveHTML($dom->getElementsByTagname('html')->item(0));
-
+$html = str_replace_json("<body>", "", $html);
+$html = str_replace_json("</body>", "", $html);	
 echo $html;
         
 
