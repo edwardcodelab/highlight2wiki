dd8f7c7a016b66d40cf17e95da21deb50044315e
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-30 00:30:32 +0800

    Update action.php

--- action.php
+++ action.php
@@ -216,7 +216,7 @@ echo '
         </form>';
 		
 		
-echo' function loadH2WFunc(){
+echo' <script>function loadH2WFunc(){
             if (document.getElementsByTagName("title")[1] !=null){
 	    var papertitle = document.getElementsByTagName("title")[1].textContent;
 		}else{
@@ -226,7 +226,7 @@ echo' function loadH2WFunc(){
            if (document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value == ""){
            document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += "====== "+papertitle+" ====== \r\n [["+url+"|" + papertitle + "]][["+highlightactionurl+"&ur="+url+"|Hightlight]]--"+timestamp+"\r\n\n";
           document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += "{{tag> }}\r\n\n"; //add tag syntax        		  
-}}	';		
+}}	</script>';		
 		
       //  <form name="testform" hidden="hidden" >
            // <textarea name="selectedtext" 
