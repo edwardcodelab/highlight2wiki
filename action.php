57b218ea80025bd713f3f12acbbe3c2ee496c1b0
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-29 22:25:03 +0800

    Update action.php

--- action.php
+++ action.php
@@ -177,7 +177,9 @@ echo '<meta Content-Type" content="text/html; charset="UTF-8">';
 $titles = $dom->saveHTML($dom->getElementsByTagName('title')->item(0));
 $html = $dom->saveHTML($dom->getElementsByTagname('body')->item(0));
 
-$html = str_replace("<body", "", $html);
+
+$html=preg_replace("/<body[^>]+\>/ix", "", $html); 
+$html = str_replace("<body>", "", $html);
 $html = str_replace("</body>", "", $html);
 
 
