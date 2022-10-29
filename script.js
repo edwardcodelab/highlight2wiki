50f481022efe5f5a097eda2786e25956022ef01a
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-29 20:34:07 +0800

    updated

--- script.js
+++ script.js
@@ -29,18 +29,13 @@
 		}else{
 	    var papertitle = url;
 		}
-	    
-
            // document.testform.selectedtext.value += selectedText +"\n";
            if (document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value == ""){
            document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += "====== "+papertitle+" ====== \r\n [["+url+"|" + papertitle + "]][["+highlightactionurl+"&ur="+url+"|Hightlight]]--"+timestamp+"\r\n\n";
           document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += "{{tag> }}\r\n\n"; //add tag syntax        
-		  document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += selectedText + "\n\n"; 
-		   console.log(document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value);		  
-		  
+		  document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += selectedText + "\n\n"; 		  
                 }
            let selectedTextString = document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value;
-           console.log(document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value);	
             document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value = selectedTextString + selectedText + "\n\n";
         }
  
