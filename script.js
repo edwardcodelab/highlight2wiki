57f6914c722dc04a2a7306061907f9b2d06df33d
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-29 19:38:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -36,9 +36,9 @@
            document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += "====== "+papertitle+" ====== \r\n [["+url+"|" + papertitle + "]][["+highlightactionurl+"&ur="+url+"|Hightlight]]--"+timestamp+"\r\n\n";
           document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value += "{{tag> }}\r\n\n"; //add tag syntax        
                 }
-           let selectedTextString = selectedText
-		   alert(selectedText);
-            document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value = document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value + selectedText + "\n\n";
+           let selectedTextString = document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value
+		   alert(selectedTextString);
+            document.getElementById("edtop").contentWindow.document.getElementsByTagName("textarea")[0].value = selectedTextString + selectedText + "\n\n";
         }
  
     function highlight2(){
