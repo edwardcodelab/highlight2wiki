5a4fa55cb7bc15c33f356e34590df7e63daf4078
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-30 13:36:29 +0800

    Update action.php

--- action.php
+++ action.php
@@ -123,6 +123,7 @@ if (function_exists('curl_init()')) //check if curl function existed
 	 // Initialize a CURL session.
 	 //$result =file_get_contents($url);
 	 $result2 =file_get_contents($url);
+	 echo $result2;
 	 if(strlen($result2)>strlen($result)){ //check length of each result
      $result = $result2;
 	 }else{
