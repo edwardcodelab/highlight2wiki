8468bea24db72e745db33c64f893c87855cce8e6
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-30 17:29:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ function edittag(){
 function loadH2WFunc(){ 
             jQuery('img').removeAttr('srcset');
 			jQuery('#wanttext img').each(function() {
-        alert($(this).attr('src'))
+        alert(jQuery(this).attr('src'))
     });
             if (document.getElementsByTagName("title")[1] !=null){
 	    var papertitle = document.getElementsByTagName("title")[1].textContent;
