9acd9eafb7e425d3c78621caa413139e09db0a71
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-10-30 15:28:26 +0800

    Update action.php

--- action.php
+++ action.php
@@ -189,7 +189,9 @@ echo '<meta Content-Type" content="text/html; charset="UTF-8">';
 $titles = $dom->saveHTML($dom->getElementsByTagName('title')->item(0));
 $html = $dom->saveHTML($dom->getElementsByTagname('body')->item(0));
 
-
+if($allow_javascript==0){ 
+$html = preg_replace('/<\s*script.+?<\s*\/\s*script.*?>/si', ' ', $html );
+}
 $html=preg_replace("/<body[^>]+\>/ix", "", $html); 
 $html = str_replace("<body>", "", $html);
 $html = str_replace("</body>", "", $html);
