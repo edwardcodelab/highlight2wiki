3382a86e4fe39df3ab2247c82bd6976609fd61ba
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-11-01 18:37:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ function edittag(){
 function loadH2WFunc(){ 
             jQuery('#wanttext *').removeAttr('height width sizes style alt sizes');
 			//jQuery('#wanttext img').jQuery('figure').removeAttr('height width sizes style alt sizes');
-			jQuery('#wanttext').each(function() {
+			jQuery('#wanttext img').each(function() {
             if (jQuery(this).attr('src').indexOf('http') > -1){
             }else{
 			var scrurl= url_host + jQuery(this).attr('src');	
